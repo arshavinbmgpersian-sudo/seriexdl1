7c27e76c5a0fc85c30c4d078c8eb74d74648d2df
arshavinbmgpersian-sudo <arshavinbmgpersian@gmail.com>
2025-10-12 16:05:08 +0330

    Delete static directory

--- static/js/plyrjsOptions.js
+++
@@ -1,8 +0,0 @@
-let player = new Plyr('video', {
-    controls:
-        ['play-large', 'play', 'progress',
-            'current-time', 'mute', 'volume', 'captions', 'settings', 'fullscreen'],
-    tooltips: {
-        controls: true,
-    },
-});
